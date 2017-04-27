687e15dfa1380349c27e27651a0d57cc6998f869
Martin <zaragozamartin91@gmail.com>
2017-04-27 12:20:44 -0300

    Se logro hacer un servicio y cliente con X509.
    El mensaje de respuesta viene encriptado, pero eso puede ser debido al
    uso de ws policies que dictan que el output encripte y firme el
    soapBody.

---
+++ CxfMavenSslX509Client/src/main/java/mz/ex/ws/BookRepoService.java
@@ -0,0 +1,72 @@
+package mz.ex.ws;
+
+import java.net.URL;
+
+import javax.xml.namespace.QName;
+import javax.xml.ws.Service;
+import javax.xml.ws.WebEndpoint;
+import javax.xml.ws.WebServiceClient;
+import javax.xml.ws.WebServiceFeature;
+
+@WebServiceClient(name = "BookRepoService", wsdlLocation = "classpath:BookRepoImpl.wsdl", targetNamespace = "http://ws.ex.mz/")
+public class BookRepoService extends Service {
+
+	public final static URL WSDL_LOCATION;
+
+	public final static QName SERVICE = new QName("http://ws.ex.mz/", "BookRepoService");
+	public final static QName BookRepoPort = new QName("http://ws.ex.mz/", "BookRepoPort");
+	static {
+		URL url = BookRepo.class.getClassLoader().getResource("BookRepoImpl.wsdl");
+		if (url == null) {
+			java.util.logging.Logger.getLogger(BookRepo.class.getName()).log(java.util.logging.Level.INFO, "Can not initialize the default wsdl from {0}",
+					"classpath:BookRepoImpl.wsdl");
+		}
+		WSDL_LOCATION = url;
+	}
+
+	public BookRepoService(URL wsdlLocation) {
+		super(wsdlLocation, SERVICE);
+	}
+
+	public BookRepoService(URL wsdlLocation, QName serviceName) {
+		super(wsdlLocation, serviceName);
+	}
+
+	public BookRepoService() {
+		super(WSDL_LOCATION, SERVICE);
+	}
+
+	public BookRepoService(WebServiceFeature... features) {
+		super(WSDL_LOCATION, SERVICE, features);
+	}
+
+	public BookRepoService(URL wsdlLocation, WebServiceFeature... features) {
+		super(wsdlLocation, SERVICE, features);
+	}
+
+	public BookRepoService(URL wsdlLocation, QName serviceName, WebServiceFeature... features) {
+		super(wsdlLocation, serviceName, features);
+	}
+
+	/**
+	 *
+	 * @return returns DoubleItPortType
+	 */
+	@WebEndpoint(name = "BookRepoPort")
+	public BookRepo getBookRepoPort() {
+		return super.getPort(BookRepoPort, BookRepo.class);
+	}
+
+	/**
+	 * 
+	 * @param features
+	 *            A list of {@link javax.xml.ws.WebServiceFeature} to configure on the proxy. Supported features not in
+	 *            the <code>features</code> parameter will have their default values.
+	 * @return returns DoubleItPortType
+	 */
+	@WebEndpoint(name = "BookRepoPort")
+	public BookRepo getBookRepoPort(WebServiceFeature... features) {
+		return super.getPort(BookRepoPort, BookRepo.class, features);
+	}
+
+}
